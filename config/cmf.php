601196318bfb0a3a925de759b0fed419577d1bb4
Rein Van Oyen <reinvanoyen@gmail.com>
2022-01-20 14:16:06 +0100

    Improved default config

--- config/cmf.php
+++ config/cmf.php
@@ -26,6 +26,7 @@ return [
     */
     'theme_image_src' => '/vendor/cmf/splash.jpg',
 
+
     /*
     |--------------------------------------------------------------------------
     | Media library disk
@@ -34,7 +35,27 @@ return [
     | The disk to use for the media library.
     |
     */
-    'media_library_disk' => config('filesystems.default'),
+    'media_library_disk' => env('CMF_DEFAULT_MEDIA_DISK', 'public'),
 
-    'guard' => 'api',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Media library file labels
+    |--------------------------------------------------------------------------
+    |
+    | The different labels the user can tag media files with. This can be used for
+    | organisational purposes or to implement differing behavior for files
+    |
+    */
+    'media_library_file_labels' => [
+        'photo' => [
+            'name' => 'Photo',
+            'color' => '#4458b3',
+        ],
+        'document' => [
+            'name' => 'Document',
+            'color' => '#f14000',
+        ],
+    ],
+
 ];
