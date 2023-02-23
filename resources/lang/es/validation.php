b383b7868aee9c4757c16078e9c817b47b645be7
EduardoMtz1 <hbkeduardo10@hotmail.com>
2023-02-23 14:04:36 -0600

    Added: Spanish translation

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'uppercase' => 'Por favor ingresa un valor en mayusculas',
+    'mimetype' => 'El :attribute seleccionado tiene el formato de archivo equivocado. (Permitido: :mimetypes)',
+];
