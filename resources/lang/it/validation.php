5b4d30b4236218a8d81fde1f00e7ef8b30cc868e
Gabriele Bellucci <gabriele@neednap.net>
2023-06-13 19:56:29 +0800

    added italian translation

---
+++ resources/lang/it/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'uppercase' => 'Inserisci un valore in maiuscolo.',
+    'mimetype' => 'Il :attribute selezionato ha un formato non consentito. (Formati consentiti: :mimetypes)',
+];
