c8bbe469258338c8a01ac33af7759ae0a770c567
Leonardo Medeiros <leonardo@jasperstudios.com>
2023-08-26 12:44:35 -0400

    Brazilian portuquese translation files

---
+++ resources/lang/pt-br/snippets.php
@@ -0,0 +1,183 @@
+<?php
+
+return [
+
+    'baseline' => 'A Base Sólida do seu Conteúdo',
+    'welcome' => 'Bem-vindo!',
+    'welcome_title' => 'Bem-vindo à sua aplicação CMF!',
+    'welcome_text' => 'Laravel CMF é o Framework de Gestão de Conteúdo flexível, para a sua próxima aplicação Laravel.',
+    'useful_resources' => 'Recursos úteis',
+
+    'offline_title' => 'Voce parece estar desconectado (offline)',
+    'offline_text' => 'Parece que voce perdeu a conexão com a internet. Se quiser continuar usando essa aplicação, por favor conecte-se com a internet.',
+    'offline_confirm' => 'Verificar minha conexão',
+    'offline_notification' => 'Opa, você ainda não está conectado!',
+    'offline_online_notification' => 'Você está conectado novamente!',
+
+    'email_address' => 'Endereço de email',
+    'password' => 'Senha',
+
+    'login' => 'Usuário',
+    'login_failed' => 'Algo deu errado ao tentar autenticar a sua conta',
+    'logout' => 'Sair',
+    'logout_confirm' => 'Sim, sair!',
+    'welcome_back' => 'Bem-vindo de volta, :name',
+
+    'cancel' => 'Cancelar',
+    'confirm' => 'Confirmar',
+    'create' => 'Criar',
+    'delete' => 'Excluir',
+    'edit' => 'Alterar',
+    'rename' => 'Renomear',
+    'upload' => 'Carregar',
+
+    'created' => 'Criado',
+
+    'next' => 'Próximo',
+    'previous' => 'Anterior',
+
+    'file' => 'Arquivo',
+    'directory' => 'Pasta',
+
+    'select_files_from_computer' => 'Selecione arquivos no seu computador',
+
+    'rename_file' => 'Renomear arquivo',
+    'rename_file_title' => 'Renomear título do arquivo',
+    'rename_file_confirm' => 'Confirma novo nome?',
+    'rename_file_cancel' => 'Cancelar',
+
+    'delete_file_title' => 'Excluir arquivo?',
+    'delete_file_text' => 'Ao excluir esse arquivo ele será permanentemente removido da sua biblioteca.',
+    'delete_file_confirm' => 'Excluir arquivo',
+    'delete_file_cancel' => 'Manter arquivo',
+
+    'delete_file' => 'Excluir arquivo',
+
+    'delete_files_title' => 'Excluir :amount arquivos?',
+    'delete_files_text' => 'Ao excluir esses arquivos eles serão permanentemente removidos da sua biblioteca. Essa ação é irreversível.',
+    'delete_files_confirm' => 'Excluir :amount arquivos',
+    'delete_files_cancel' => 'Manter arquivos',
+
+    'file_renamed' => 'Arquivo renomeado',
+    'directory_created' => 'Pasta criada',
+
+    'media_library' => 'Biblioteca de media',
+    'files_root' => 'Meus arquivos',
+
+    'new_directory' => 'Novo arquivo',
+    'new_directory_title' => 'Criar novo arquivo',
+
+    'copied_to_clipboard' => 'Copiado para a área de transferência',
+
+    'update_slug' => 'Alterar slug',
+
+    'search' => 'Buscar',
+    'clear_filters' => 'Limpar filtros',
+
+    'tag' => 'Tag',
+    'tags_all' => 'Todos',
+    'selected_tags' => 'Selecionar tags',
+    'available_tags' => 'Tags disponíveis',
+
+    'order_changed' => 'Ordem foi alterada',
+
+    'no_plural_found' => 'Nenhum :plural foram encontrados.',
+    'no_plural_found_for_search' => 'Nenhum :plural foram encontrados para essa busca ":search."',
+    'no_singular_selected' => 'Nenhum :singular selecionado',
+    'no_plural_selected' => 'Nenhum :plural selecionados',
+    'no_plural_added' => 'Nenhum :plural foram adicionados ainda',
+
+    'delete_singular' => 'Excluir :singular',
+    'save_singular' => 'Salvar :singular',
+    'create_singular' => 'Criar :singular',
+    'new_singular' => 'Novo :singular',
+    'select_singular' => 'Selecionar :singular',
+    'add_singular' => 'Adicionar :singular',
+
+    'select_plural' => 'Selecionar :plural',
+    'add_plural' => 'Adicionar :plural',
+
+    'delete_singular_title' => 'Excluir :singular?',
+    'delete_singular_text' => 'Tem certeza que quer excluir esse :singular?',
+    'delete_singular_confirm' => 'Excluir :singular',
+    'delete_singular_cancel' => 'Cancelar',
+
+    'singular_created' => ':singular criado',
+    'singular_updated' => ':singular atualizado',
+    'singular_deleted' => ':singular excluído',
+    'singular_inserted' => ':singular inserido',
+    'singular_added' => ':singular adicionado',
+    'singular_created_selected' => ':singular criado e selecionado',
+
+    'form_has_errors' => 'Algo deu errado com esse formulário',
+
+    'directory_is_empty' => 'Essa pasta está vazia',
+    'unknown_filetype' => 'Tipo de arquivo desconhecido',
+    'no_label_selected' => 'Nenhuma etiqueta selecionada',
+    'uploaded' => 'Carregado',
+    'modified' => 'Modificado',
+    'description' => 'Descrição',
+    'copyright' => 'Copyright',
+    'filesize' => 'Tamanho',
+    'mimetype' => 'Mimetype',
+    'visibility' => 'Visibilidade',
+    'disk' => 'Disco',
+    'conversions_disk' => 'Disco de conversão',
+
+    'move' => 'Mover',
+    'download' => 'Baixar',
+
+    'open' => 'Abrir',
+    'open_new_window' => 'Abrir em nova janela',
+
+    'move_files' => 'Mover :amount arquivos',
+    'delete_files' => 'Excluir :amount arquivos',
+    'cant_move_directory_inside_self' => 'Impossível mover uma pasta para ela mesma',
+
+    'move_files_text' => 'Tem certeza que quer mover os arquivos para cá?',
+    'move_files_confirm' => 'Mova os arquivos',
+    'move_files_cancel' => 'Cancelar',
+
+    'none' => 'Nenhum',
+    'changes_successful' => 'Alterações salvas',
+    'changes_unsuccessful' => 'Não foi possível salvar as alterações',
+
+    'save' => 'Salvar',
+
+    'directory_renamed' => 'Nome da pasta alterado',
+    'directory_moved' => 'Pasta movida',
+    'directory_deleted' => 'Pasta excluída',
+    'directory_not_deleted' => 'Não foi possível excluir a pasta',
+
+    'file_moved' => 'Arquivo movido',
+    'file_deleted' => 'Arquivo excluído',
+    'files_moved' => 'Arquivos movidos',
+    'file_not_deleted' => 'Não foi possível excluir o arquivo',
+    'amount_files_deleted' => ':amount arquivos excluídos',
+    'files_not_deleted' => 'Não foi possível excluir os arquivos',
+
+    'delete_directory' => 'Excluir pasta',
+    'delete_directory_title' => 'Excluir pasta?',
+    'delete_directory_text' => 'Ao excluir essa pasta, todos os arquivos contidos nela também serão excluídos da biblioteca de media.',
+    'delete_directory_confirm' => 'Sim, exclua a pasta',
+    'delete_directory_cancel' => 'Cancelar',
+
+    'jump_to_folder' => 'Ir para pasta',
+    'deselect' => 'Deselecionar',
+    'your_selection_is_empty' => 'Nenhum ítem selecionado',
+    'confirm_selection' => 'Confirmar seleção',
+    'select_file' => 'Selecionar um arquivo',
+    'your_selection' => 'Sua seleção',
+
+    'files_uploaded' => 'Seus arquivos foram carregados',
+
+    'insert_singular_below' => 'Inserir :singular abaixo',
+
+    'tags' => 'Tags',
+    'no_tags_available' => 'Nenhuma tag disponível',
+
+    'reloaded_plural' =>  ':plural recarregados',
+
+    'edit_button_text' => 'Texto do botão de edição',
+    'edit_button_url' => 'URL do botão de edição',
+];
---
+++ resources/lang/pt-br/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'uppercase' => 'Por favor informe um valor em caixa-alta.',
+    'mimetype' => 'O atributo :attribute selecionado está com usando um formato de arquivo não suportado. (Permitidos: :mimetypes)',
+];
