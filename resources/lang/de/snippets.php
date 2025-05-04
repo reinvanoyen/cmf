c4ac2dd049238e91a11fcf05ecc628754842b2d0
dbto <jens.lechner.dbto@googlemail.com>
2025-05-04 13:32:58 +0200

    add german translation

---
+++ resources/lang/de/snippets.php
@@ -0,0 +1,186 @@
+<?php
+
+return [
+
+    'baseline' => 'Das solide Fundament für Ihre Inhalte',
+    'welcome' => 'Willkommen',
+    'welcome_title' => 'Willkommen in Ihrer CMF-Anwendung!',
+    'welcome_text' => 'Laravel CMF ist das flexible Content-Management-Framework für Ihre nächste Laravel-Anwendung.',
+    'useful_resources' => 'Nützliche Ressourcen',
+
+    'offline_title' => 'Sieht so aus, als wären Sie offline',
+    'offline_text' => 'Es scheint, dass Sie die Verbindung zum Internet verloren haben. Wenn Sie diese Anwendung weiterhin nutzen möchten, stellen Sie bitte eine Verbindung zum Internet her.',
+    'offline_confirm' => 'Prüfe meine Verbindung',
+    'offline_notification' => 'Oops, Sie sind immer noch offline!',
+    'offline_online_notification' => 'Sie sind wieder online!',
+
+    'email_address' => 'E-Mail-Adresse',
+    'password' => 'Passwort',
+
+    'login' => 'Anmelden',
+    'login_failed' => 'Etwas ist beim Einloggen schiefgelaufen',
+    'logout' => 'Abmelden',
+    'logout_confirm' => 'Ja, abmelden!',
+    'welcome_back' => 'Willkommen zurück, :name',
+
+    'cancel' => 'Abbrechen',
+    'confirm' => 'Bestätigen',
+    'create' => 'Erstellen',
+    'delete' => 'Löschen',
+    'edit' => 'Bearbeiten',
+    'rename' => 'Umbenennen',
+    'upload' => 'Hochladen',
+
+    'created' => 'Erstellt',
+
+    'next' => 'Nächste',
+    'previous' => 'Vorherige',
+
+    'file' => 'Datei',
+    'directory' => 'Ordner',
+
+    'select_files_from_computer' => 'Dateien von Ihrem Computer auswählen',
+
+    'rename_file' => 'Datei umbenennen',
+    'rename_file_title' => 'Datei umbenennen',
+    'rename_file_confirm' => 'Datei umbenennen',
+    'rename_file_cancel' => 'Abbrechen',
+
+    'delete_file_title' => 'Datei löschen?',
+    'delete_file_text' => 'Das Löschen dieser Datei entfernt sie dauerhaft aus Ihrer Bibliothek.',
+    'delete_file_confirm' => 'Datei löschen',
+    'delete_file_cancel' => 'Datei behalten',
+
+    'delete_file' => 'Datei löschen',
+
+    'delete_files_title' => 'Löschen von :amount Dateien?',
+    'delete_files_text' => 'Das Löschen dieser Dateien entfernt sie dauerhaft aus Ihrer Mediathek. Diese Aktion kann nicht rückgängig gemacht werden.',
+    'delete_files_confirm' => 'Löschen von :amount Dateien',
+    'delete_files_cancel' => 'Dateien behalten',
+
+    'file_renamed' => 'Datei umbenannt',
+    'directory_created' => 'Ordner erstellt',
+
+    'media_library' => 'Mediathek',
+    'files_root' => 'Meine Dateien',
+
+    'new_directory' => 'Neuer Ordner',
+    'new_directory_title' => 'Neuen Ordner erstellen',
+
+    'copied_to_clipboard' => 'In die Zwischenablage kopiert',
+
+    'update_slug' => 'Slug aktualisieren',
+
+    'search' => 'Suche',
+    'clear_filters' => 'Filter zurücksetzen',
+
+    'tag' => 'Tag',
+    'tags_all' => 'Alle',
+    'selected_tags' => 'Ausgewählte Tags',
+    'available_tags' => 'Verfügbare Tags',
+
+    'order_changed' => 'Reihenfolge geändert',
+
+    'no_plural_found' => 'Es wurden keine :plural gefunden.',
+    'no_plural_found_for_search' => 'Es wurden keine :plural gefunden für ":search"',
+    'no_singular_selected' => 'Keine :singular ausgewählt',
+    'no_plural_selected' => 'Keine :plural ausgewählt',
+    'no_plural_added' => 'Es wurden noch keine :plural hinzugefügt',
+
+    'delete_singular' => 'Lösche :singular',
+    'save_singular' => 'Speicher :singular',
+    'create_singular' => 'Erstelle :singular',
+    'new_singular' => 'Neues :singular',
+    'select_singular' => 'Wähle :singular',
+    'add_singular' => 'Füge :singular hinzu',
+
+    'select_plural' => 'Wähle :plural',
+    'add_plural' => 'Füge :plural hinzu',
+
+    'delete_singular_title' => 'Lösche :singular?',
+    'delete_singular_text' => 'Sind Sie sicher, dass Sie :singular löschen möchten?',
+    'delete_singular_confirm' => 'Ja, :singular löschen',
+    'delete_singular_cancel' => 'Abbrechen',
+
+    'select_singular_confirm' => 'Wähle :singular',
+    'select_singular_cancel' => 'Abbrechen',
+
+    'singular_created' => ':singular wurde erstellt',
+    'singular_updated' => ':singular wurde aktualisiert',
+    'singular_deleted' => ':singular wurde gelöscht',
+    'singular_inserted' => ':singular wurde eingefügt',
+    'singular_added' => ':singular wurde hinzugefügt',
+    'singular_created_selected' => ':singular wurde erstellt und ausgewählt',
+
+    'form_has_errors' => 'Etwas ist mit dem Formular schiefgelaufen',
+
+    'directory_is_empty' => 'Dieser Ordner ist leer',
+    'unknown_filetype' => 'Unbekannter Dateityp',
+    'no_label_selected' => 'Kein Label ausgewählt',
+    'uploaded' => 'Hochgeladen',
+    'modified' => 'Geändert',
+    'description' => 'Beschreibung',
+    'copyright' => 'Urheberrecht',
+    'filesize' => 'Größe',
+    'mimetype' => 'Mimetype',
+    'visibility' => 'Sichtbarkeit',
+    'disk' => 'Disk',
+    'conversions_disk' => 'Konvertierungsdisk',
+
+    'move' => 'Verschieben',
+    'download' => 'Herunterladen',
+
+    'open' => 'Öffnen',
+    'open_new_window' => 'Öffnen in neuem Fenster',
+
+    'move_files' => 'Verschiebe :amount Dateien',
+    'delete_files' => 'Lösche :amount Dateien',
+    'cant_move_directory_inside_self' => 'Kann Ordner nicht in sich selbst verschieben',
+
+    'move_files_text' => 'Sind Sie sicher, dass Sie Ihre Dateien hierher verschieben möchten?',
+    'move_files_confirm' => 'Ja, Dateien verschieben',
+    'move_files_cancel' => 'Abbrechen',
+
+    'none' => 'Keine',
+    'changes_successful' => 'Änderungen erfolgreich gespeichert',
+    'changes_unsuccessful' => 'Änderungen konnten nicht gespeichert werden',
+
+    'save' => 'Speichern',
+
+    'directory_renamed' => 'Ordner umbenannt',
+    'directory_moved' => 'Ordner wurde verschoben',
+    'directory_deleted' => 'Ordner wurde gelöscht',
+    'directory_not_deleted' => 'Ordner konnte nicht gelöscht werden',
+
+    'file_moved' => 'Datei wurde verschoben',
+    'file_deleted' => 'Datei wurde gelöscht',
+    'files_moved' => 'Dateien wurden verschoben',
+    'file_not_deleted' => 'Datei konnte nicht gelöscht werden',
+    'amount_files_deleted' => ':amount Dateien wurden gelöscht',
+    'files_not_deleted' => 'Dateien konnten nicht gelöscht werden',
+
+    'delete_directory' => 'Ordner löschen',
+    'delete_directory_title' => 'Ordner löschen?',
+    'delete_directory_text' => 'Das Löschen dieses Ordners entfernt ihn und alle seine Inhalte dauerhaft aus Ihrer Mediathek.',
+    'delete_directory_confirm' => 'Ja, Ordner löschen',
+    'delete_directory_cancel' => 'Ordner behalten',
+
+    'jump_to_folder' => 'Springe zu Ordner',
+    'deselect' => 'Abwählen',
+    'your_selection_is_empty' => 'Sie haben nichts ausgewählt',
+    'confirm_selection' => 'Bestätige Auswahl',
+    'select_file' => 'Wähle eine Datei',
+    'your_selection' => 'Ihre Auswahl',
+
+    'files_uploaded' => 'Ihre Dateien wurden hochgeladen',
+
+    'insert_singular_below' => 'Füge :singular darunter ein',
+
+    'tags' => 'Tags',
+    'no_tags_available' => 'Keine Tags verfügbar',
+
+    'reloaded_plural' => ':plural wurden neu geladen',
+
+    'edit_button_text' => 'Bearbeiten Sie den Schaltflächentext',
+    'edit_button_url' => 'Bearbeiten Sie die Schaltflächen-URL',
+];
---
+++ resources/lang/de/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'uppercase' => 'Bitte geben Sie einen Wert in Großbuchstaben ein.',
+    'mimetype' => 'Das ausgewählte :attribute hat ein ungültiges Format. (Erlaubte Formate: :mimetypes)',
+];
